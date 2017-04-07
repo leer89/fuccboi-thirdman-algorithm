d87a4e13e28bc16ff5df6b6492a2b793fbc4d1ae
leer89 <leer89@gmail.com>
2017-04-06 21:59:56 -0400

    v1 fuccboi using lists

--- fuccboi-thirdman-algorithm/fuccboi-thirdman-algorithm.cpp
+++ fuccboi-thirdman-algorithm/fuccboi-thirdman-algorithm.cpp
@@ -3,9 +3,39 @@
 
 #include "stdafx.h"
 
+#include <iostream>
+#include <list>
 
-int main()
-{
-    return 0;
+using namespace std;
+
+int main() {
+	int i;
+	int numOfSuitors;
+	list<int> suitors;
+	cout << "How many boys came over? : ";
+	cin >> numOfSuitors;
+
+	// adds element to list
+	for (i = 0; i < numOfSuitors; i++)
+		suitors.push_back(i);
+
+	// kill every 3rd fuccboi
+	int killThisGuy = 3;
+	i = killThisGuy;
+	list<int>::iterator it = suitors.begin();
+
+	// loop until 1 fuccboi remains
+	while (suitors.size() > 1) {
+		if (--i == 0) {
+			// check to see if you gotta kill the third fuccboi
+			list<int>::iterator suitorsPointer = it++; // move iterator 1 pointer 
+			suitors.erase(suitorsPointer); // kill suitor
+			i = killThisGuy; // re-arm the counter
+		}
+		else it++; // was not third, simply goes on
+
+		if (it == suitors.end()) it = suitors.begin(); // if at end, go back to begin
+	}
+	cout << "The last fuccboi is dude number: " << suitors.front() + 1 << endl; // list started at 0
+	return 0;
 }
-
