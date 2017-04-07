428d0faafbfb5ecbcb2a4cdf72e6fd415c24bbae
leer89 <leer89@gmail.com>
2017-04-06 22:05:10 -0400

    family friendly variable name changes and cout statements

--- fuccboi-thirdman-algorithm/fuccboi-thirdman-algorithm.cpp
+++ fuccboi-thirdman-algorithm/fuccboi-thirdman-algorithm.cpp
@@ -1,5 +1,7 @@
-// fuccboi-thirdman-algorithm.cpp : Defines the entry point for the console application.
-//
+
+// Author : Randy Lee
+// Professor Dada
+// 2017-04-06
 
 #include "stdafx.h"
 
@@ -12,30 +14,30 @@ int main() {
 	int i;
 	int numOfSuitors;
 	list<int> suitors;
-	cout << "How many boys came over? : ";
+	cout << "How many people want to try their luck? : ";
 	cin >> numOfSuitors;
 
 	// adds element to list
 	for (i = 0; i < numOfSuitors; i++)
 		suitors.push_back(i);
 
-	// kill every 3rd fuccboi
+	// kill every 3rd person
 	int killThisGuy = 3;
 	i = killThisGuy;
 	list<int>::iterator it = suitors.begin();
 
-	// loop until 1 fuccboi remains
+	// loop until 1 person remains
 	while (suitors.size() > 1) {
 		if (--i == 0) {
-			// check to see if you gotta kill the third fuccboi
+			// check to see if you gotta kill the third person
 			list<int>::iterator suitorsPointer = it++; // move iterator 1 pointer 
 			suitors.erase(suitorsPointer); // kill suitor
 			i = killThisGuy; // re-arm the counter
 		}
-		else it++; // was not third, simply goes on
+		else it++; // if the current suitor is not 3rd, move along
 
 		if (it == suitors.end()) it = suitors.begin(); // if at end, go back to begin
 	}
-	cout << "The last fuccboi is dude number: " << suitors.front() + 1 << endl; // list started at 0
+	cout << "Last man standing : " << suitors.front() + 1 << endl; // list started at 0
 	return 0;
 }
